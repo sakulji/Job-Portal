317b7ade175c818ab8b782c51a3ae98d7207a72a
Sakulji Pulugu <sakuljipulugu@gmail.com>
2026-01-12 13:03:57 +0530

    Delete src/main directory

--- src/main/java/com/jobportal/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.jobportal.repository;
-import java.util.Optional;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.jobportal.model.User;
-
-public interface UserRepository extends MongoRepository<User,String> {
- Optional<User> findByEmail(String email);
-}
